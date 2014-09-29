acc9ba2b8762c9df4439bdd7a2fff4f214c8695d
Junya Wako <j.wako@j-q.co.jp>
2014-09-29 14:27:42 +0900

    Fix bugs in notification

--- app/controllers/approves_controller.rb
+++ app/controllers/approves_controller.rb
@@ -4,7 +4,7 @@ class ApprovesController < ApplicationController
     token = params[:token] if params[:token]
     if token
       notification = Notification.find_by(token: token)
-      notification.update_attributes!(active: true, token: nil) if notification
+      notification.update_attributes!(active: true) if notification
     end
     return redirect_to root_path, alert: "該当する通知が存在しません。" if token.nil? || notification.nil?
   end
